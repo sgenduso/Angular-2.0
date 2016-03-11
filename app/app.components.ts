872b7f3d8c37069b1c4f00f5825e2750f4300f0b
Sam Genduso <sam.genduso@gmail.com>
2016-03-10 18:17:51 -0700

    main.ts

--- app/app.components.ts
+++ app/app.components.ts
@@ -1,7 +1,10 @@
+// import statements reference the things we need.
 import {Component} from 'angular2/core';
 
+// @Component tells Angular what template to use and how to create the component.
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
+// component class controls the appearance and behavior of a view through its template.
 export class AppComponent { }
