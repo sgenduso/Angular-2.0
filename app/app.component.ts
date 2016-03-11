6e55f6edd4cfcb7c385228e3b47bbadcd446d109
Sam Genduso <sam.genduso@gmail.com>
2016-03-10 18:55:18 -0700

    interfacing and two way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,29 @@
 // import statements reference the things we need.
 import {Component} from 'angular2/core';
 
+interface Hero {
+  id: number;
+  name: string;
+}
+
 // @Component tells Angular what template to use and how to create the component.
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+      </div>
+      `
 })
 // component class controls the appearance and behavior of a view through its template.
-export class AppComponent { }
+export class AppComponent {
+  public title = 'Tour of Heroes';
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
