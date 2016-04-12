741bad913de8094bbcac48c139a73f658c548fcc
Sam Genduso <sam.genduso@gmail.com>
2016-04-11 18:00:22 -0600

    hero detail component template

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -5,16 +5,7 @@ import { HeroService } from './hero.service';
 import { Component, Input, OnInit } from 'angular2/core';
 @Component({
   selector: 'my-hero-detail',
-  template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-`,
+  templateUrl: 'app/hero-detail.component.html',
 inputs: ['hero']
 })
 ngOnInit() {
