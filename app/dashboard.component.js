662fc3f6994b6768f807728ca5e4778e7549dbc7
Sam Genduso <sam.genduso@gmail.com>
2016-04-12 18:44:59 -0600

    dashboard router added

--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero.service'], function(exports_1, context_1) {
+System.register(['angular2/core', './hero.service', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './hero.service'], function(exports_1, context
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_service_1;
+    var core_1, hero_service_1, router_1;
     var DashboardComponent;
     return {
         setters:[
@@ -19,10 +19,14 @@ System.register(['angular2/core', './hero.service'], function(exports_1, context
             },
             function (hero_service_1_1) {
                 hero_service_1 = hero_service_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             DashboardComponent = (function () {
-                function DashboardComponent(_heroService) {
+                function DashboardComponent(_router, _heroService) {
+                    this._router = _router;
                     this._heroService = _heroService;
                     this.heroes = [];
                 }
@@ -31,13 +35,16 @@ System.register(['angular2/core', './hero.service'], function(exports_1, context
                     this._heroService.getHeroes()
                         .then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
                 };
-                DashboardComponent.prototype.gotoDetail = function () { };
+                DashboardComponent.prototype.gotoDetail = function (hero) {
+                    var link = ['HeroDetail', { id: hero.id }];
+                    this._router.navigate(link);
+                };
                 DashboardComponent = __decorate([
                     core_1.Component({
                         selector: 'my-dashboard',
                         templateUrl: 'app/dashboard.component.html',
                     }), 
-                    __metadata('design:paramtypes', [hero_service_1.HeroService])
+                    __metadata('design:paramtypes', [router_1.Router, hero_service_1.HeroService])
                 ], DashboardComponent);
                 return DashboardComponent;
             }());
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,20 +1,27 @@
-import {Component} from 'angular2/core';
-import {Hero} from './hero';
-import { RouteParams } from 'angular2/router';
-import { HeroService } from './hero.service';
 import { Component, Input, OnInit } from 'angular2/core';
+import { RouteParams } from 'angular2/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 @Component({
   selector: 'my-hero-detail',
   templateUrl: 'app/hero-detail.component.html',
-inputs: ['hero']
 })
-ngOnInit() {
-   let id = +this._routeParams.get('id');
-   this._heroService.getHero(id)
-     .then(hero => this.hero = hero);
- }
 export class HeroDetailComponent implements OnInit {
-  hero: Hero;
+  @Input() hero: Hero;
+
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams) {
+  }
+
+  ngOnInit() {
+    let id = +this._routeParams.get('id');
+    this._heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  }
+
   goBack() {
     window.history.back();
   }
